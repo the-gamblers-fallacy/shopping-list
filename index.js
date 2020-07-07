0056767bd26508976edf8f4f392d4099a34ba949
Lucas Bigelow <lukejbigelow@gmail.com>
2020-07-07 15:14:35 -0500

    implemented the shopping list item add functionality

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
+function createShoppingItem(item) {
+  return $(`
+    <li>
+      <span class="shopping-item">${item}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+  `);
+}
+
 function main() {
-  // and so it begins
+  $('button').on('click', function(evt) {
+    evt.preventDefault();
+    let userItem = $('input').val();
+    $('.shopping-list').append(createShoppingItem(userItem));
+    $('input').empty();
+  });
 }
 
 $(main);
